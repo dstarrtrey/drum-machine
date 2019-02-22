2eeb28165d6f6806729bf408cc0480840defc86c
David Starr <dstarrtrey@gmail.com>
2019-02-22 15:16:51 -0800

    Adds gh pages functionality

--- src/index.js
+++ src/index.js
@@ -1,15 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import "./style.css";
+import NowNowNow from "./audio/nownownow.wav";
+import SpeakingOfIMDB from "./audio/speakingofimdb.wav";
+import DicksPicks from "./audio/dickspicks.wav";
+import PremiumPickles from "./audio/premiumpickles.wav";
+import For from "./audio/for.wav";
+import YourTickles from "./audio/yourtickles.wav";
+import BuyBuyBuy from "./audio/buybuybuy.wav";
+import ItWasLong from "./audio/itwaslong.wav";
 import Audio from "./audio/friggenheavymate.wav";
 function Pad(props) {
   function handleClick() {
     console.log("keyBind: ", props.keyBind);
-    document.getElementById(props.keyBind).play();
+    const myAudio = document.getElementById(props.keyBind);
+    myAudio.currentTime = 0;
+    myAudio.volume = JSON.parse(props.vol);
+    myAudio.play();
     console.log("Source: ", Audio);
     console.log("Pad props: ", props);
     props.buttonClick(props.audioName);
-
   }
   return (
     <button
@@ -35,10 +45,10 @@ class App extends React.Component {
       pressed: "Made with React"
     };
   }
-  changeState(event){
+  changeState(event) {
     console.log("Event: ", event);
     console.log("This: ", this);
-    this.setState({pressed: event})
+    this.setState({ pressed: event });
   }
   render() {
     return (
@@ -46,15 +56,69 @@ class App extends React.Component {
         <h1>Three Men and a Movie Soundboard</h1>
         <Display text={this.state.pressed} />
         <div className="pads">
-          <Pad buttonClick={e => this.changeState(e)} keyBind="Q" audio={Audio} audioName="FriggenHeavyMate 1" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="W" audio={Audio} audioName="FriggenHeavyMate 2" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="E" audio={Audio} audioName="FriggenHeavyMate 3" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="A" audio={Audio} audioName="FriggenHeavyMate 4" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="S" audio={Audio} audioName="FriggenHeavyMate 5" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="D" audio={Audio} audioName="FriggenHeavyMate 6" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="Z" audio={Audio} audioName="FriggenHeavyMate 7" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="X" audio={Audio} audioName="FriggenHeavyMate 8" />
-          <Pad buttonClick={e => this.changeState(e)} keyBind="C" audio={Audio} audioName="FriggenHeavyMate 9" />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="1.0"
+            keyBind="Q"
+            audio={NowNowNow}
+            audioName="NOW NOW NOW"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="1.0"
+            keyBind="W"
+            audio={SpeakingOfIMDB}
+            audioName="Speaking of IMDB"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="0.8"
+            keyBind="E"
+            audio={DicksPicks}
+            audioName="Dick's Picks"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="0.6"
+            keyBind="A"
+            audio={PremiumPickles}
+            audioName="Premium Pickles"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="0.6"
+            keyBind="S"
+            audio={For}
+            audioName="For"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="0.6"
+            keyBind="D"
+            audio={YourTickles}
+            audioName="Your Tickles"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="1.0"
+            keyBind="Z"
+            audio={BuyBuyBuy}
+            audioName="BUY THESE PICKLES PLEASE"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="0.8"
+            keyBind="X"
+            audio={ItWasLong}
+            audioName="It Was Long"
+          />
+          <Pad
+            buttonClick={e => this.changeState(e)}
+            vol="0.2"
+            keyBind="C"
+            audio={Audio}
+            audioName="FriggenHeavyMate MIDI"
+          />
         </div>
         <footer>© David L. Starr 2019</footer>
       </div>
